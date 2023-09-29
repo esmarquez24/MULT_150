78996bed289dfbd4778a6dd09b3de223f670c2c2
dollx <141386867+cyberneticdollx@users.noreply.github.com>
2023-09-28 19:58:08 -0700

    tryityourself in the works

---
+++ emarquez_hour08/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hVal = Input.GetAxis("Horizontal");
+        float vVal = Input.GetAxis("Vertical");
+
+        if (hVal != 0)
+            print("Horizontal movement selected: " + hVal);
+        if (vVal != 0)
+            print("Vertical movement selected: " + vVal);
+
+        if (Input.GetKey(KeyCode.M))
+            print("The 'M' key is pressed down");
+        if (Input.GetKeyDown(KeyCode.O))
+            print("The 'O' key is pressed down");
+    }
+}
