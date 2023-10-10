4076196416885b8c776e687e37944b7458c91184
dollx <esmarquez24@gmail.com>
2023-10-10 00:33:13 -0700

    hour 09 cont

--- emarquez_hour09/Assets/Scripts/MotionScript.cs
+++ emarquez_hour09/Assets/Scripts/MotionScript.cs
@@ -10,9 +10,21 @@ public class MotionScript : MonoBehaviour
         
     }
 
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
     // Update is called once per frame
     void Update()
     {
-        
+        float mX = Input.GetAxis("Mouse X") / 10;
+        float mY = Input.GetAxis("Mouse Y") /10;
+
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(mX, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
     }
 }
