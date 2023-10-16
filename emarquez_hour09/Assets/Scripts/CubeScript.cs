d028ff779621d50bda67ccc05e2b26efc65ead03
dollx <esmarquez24@gmail.com>
2023-10-15 23:09:32 -0700

    hour09

--- emarquez_hour09/Assets/Scripts/CubeScript.cs
+++ emarquez_hour09/Assets/Scripts/CubeScript.cs
@@ -4,7 +4,10 @@ using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class CubeScript : MonoBehaviour
+
 {
+    public static int Score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +20,11 @@ public class CubeScript : MonoBehaviour
         
     }
 
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "Sphere")
+        {
+            int v = Score++;
+        }
+    }
 }
