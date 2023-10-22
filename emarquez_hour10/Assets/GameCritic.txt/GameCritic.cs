d570a2e775c90b2539f4de2a59e4cbf9c4680d18
dollx <esmarquez24@gmail.com>
2023-10-21 22:12:12 -0700

    hour 10

--- emarquez_hour10/Assets/GameCritic.txt/GameCritic.cs
+++ emarquez_hour10/Assets/GameCritic.txt/GameCritic.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class GameCritic : MonoBehaviour
 {
    /* 
-   Is the game too easy or hard? - too easy
+   Is the game too easy or hard? - the game is too easy
    What would make it easier or harder? - adding goals the player has to achieve to win/complete the game
-   What would give it a "wow" factor? - sound effects
-   What parts of the game are fun? - Seeing which balls go into their goals first
-   What parts of the game are tedious? - Not being able to see when a ball goes into the goal if you are turned away
+   What would give it a "wow" factor? - sound effects for every time a ball goes into its respective goal & when the game is complete
+   What parts of the game are fun? - moving around
+   What parts of the game are tedious? - not being able to do much, it's more like waiting for the game to end
    */
 }
