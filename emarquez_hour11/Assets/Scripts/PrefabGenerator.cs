41f0014aa30541213d67df0cfd13b59faa381350
dollx <esmarquez24@gmail.com>
2023-10-28 22:15:12 -0700

    hour 11

--- emarquez_hour11/Assets/Scripts/PrefabGenerator.cs
+++ emarquez_hour11/Assets/Scripts/PrefabGenerator.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
 {
+    public GameObject prefab;
+    public Vector3 spawnValues;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 1; i <= 10; i++)
+        {
+            Vector3 spawnposition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0f, 0f);
+            GameObject gameObject = Instantiate<GameObject>(prefab, spawnposition, Quaternion.identity);
+        }
     }
 
-    public GameObject prefab;
     // Update is called once per frame
     void Update()
     {
