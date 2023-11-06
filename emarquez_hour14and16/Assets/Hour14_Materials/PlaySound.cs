7a4bbeca01bdbda494bae83586d2a4bbdd2d1388
dollx <esmarquez24@gmail.com>
2023-11-06 13:25:10 -0800

    hour 14 and 16

--- emarquez_hour14and16/Assets/Hour14_Materials/PlaySound.cs
+++ emarquez_hour14and16/Assets/Hour14_Materials/PlaySound.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlaySound : MonoBehaviour
 {
-    public AudioSource soundPlayer;
+    private AudioSource soundPlayer;
     public void playThisSoundEffect()
     {
-        soundPlayer.Play();
+        Debug.Log(soundPlayer);
+        soundPlayer.PlayDelayed(0);
+        SceneManager.LoadScene("Game");
+        Debug.Log(soundPlayer);
     }
+    void Awake()
+    {
+        soundPlayer = GetComponent<AudioSource>();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(soundPlayer);
     }
 
     // Update is called once per frame
